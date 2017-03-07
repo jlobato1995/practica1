f8ee737aa71cb29db2907b1e739be72135522f08
jlobato1995 <jlobato1995@gmail.com>
2017-03-07 10:12:33 +0100

    Add files via upload

---
+++ GitAux.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prgitejemplo;
+
+/**
+ *
+ * @author bertoa
+ */
+public class GitAux {
+    
+    public void metodoAlumno1(){
+               // soy master
+        System.out.println("metodo 1");
+    }
+    
+    public void metodoAlumno2(){
+             //soy auxiliar
+        System.out.println("metodo 2");
+    }
+    
+    public void metodoComunitario(){
+//soy master
+
+               //hola soy auxiliar
+        System.out.println("Aqui escribimos todos");
+              //soy auxiliar
+    }
+}
